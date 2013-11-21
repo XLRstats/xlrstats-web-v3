88f9257feaea422c23cace69e2d8a60aac5f4ef9
Freelander <freelander00@gmail.com>
2013-11-21 22:17:05 +0200

    fall back to debug level 1

--- app/Config/core.php
+++ app/Config/core.php
@@ -46,7 +46,7 @@ if (!is_writable(dirname(APP . 'tmp' . DS . 'test'))) {
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 1);
 
 /**
  * Configure the Error handler used to handle errors for your application.  By default
