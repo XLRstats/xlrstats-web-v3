9b6062a95e741e190fb0edbef7f90c8f7ae6fd90
ostoffel <ostoffel@isp-controle.de>
2014-04-01 22:30:01 +0200

    Descriptions on Weapons and Vehicles
    write some more descriptions for Weapons and Vehicles

--- app/Config/games/bf4.php
+++ app/Config/games/bf4.php
@@ -64,7 +64,7 @@ $config = array(
 		'image_path' => '/img/bf4/',
 
 		// Assault Rifles
-		'U_GalilACE23' => array('ACE 23', 'description', 'galil_ace23_fancy.png','http://symthic.com/bf4-weapon-info?w=ACE_23'),
+		'U_GalilACE23' => array('ACE 23', 'The ACE 23 is a weapon featured in Battlefield 4 for the Assault class.<br />It is unlocked upon completing the Assault Expert gold tier assignment.<br />It functions similarly to the M16A3 from Battlefield 3, coming closest <br />to its 800 RPM from the automatic weapons, having a firerate of 770 RPM,<br />making it one of the better assault rifles to be used in close quarters.<br />At longer ranges, more controlled bursts are recommended to maintain accuracy.<br />The addition of stability-enhancing attachments such as the Muzzle Brake<br />coupled with Stubby Grip; or Angled Grip with <b>Heavy Barrel</b> greatly extend<br />the weapon\'s effectiveness	beyond close and even mid-ranges.', 'galil_ace23_fancy.png','http://symthic.com/bf4-weapon-info?w=ACE_23'),
 		'U_SteyrAug' => array('AUG A3', 'description', 'steyraug_fancy.png','http://symthic.com/bf4-weapon-info?w=AUG_A3'),
 		'U_CZ805' => array('CZ-805', 'description', 'cz805_fancy.png','http://symthic.com/bf4-weapon-info?w=CZ-805'),
 		'U_FAMAS' => array('FAMAS', 'description', 'famas_fancy.png','http://symthic.com/bf4-weapon-info?w=FAMAS'),
@@ -79,7 +79,6 @@ $config = array(
 		'U_F2000' => array('F2000', 'description', 'f2000_fancy.png','http://symthic.com/bf4-weapon-info?w=F2000'),
 		'U_AR160' => array('AR 160', 'description', 'ar160_fancy.jpg','http://symthic.com/bf4-weapon-info?w=AR-160'),
 		
-
 		// Underslug
 		'U_AEK971_M320_HE' => array('AEK-971 M320 HE', 'description', 'aek971_fancy.png', 'http://symthic.com/bf4-weapon-info?w=AEK-971'),
 		'U_AEK971_M320_LVG' => array('AEK-971 M320 LVG', 'description', 'aek971_fancy.png', 'http://symthic.com/bf4-weapon-info?w=AEK-971'),
@@ -92,14 +91,15 @@ $config = array(
 		'U_SteyrAug_M320_FLASH' => array('AUG A3 M320 Flashbang', 'description', 'steyraug_fancy.png','http://symthic.com/bf4-weapon-info?w=AUG_A3'),
 		'U_SteyrAug_M320_HE' => array('AUG A3 M320 HE', 'description', 'steyraug_fancy.png','http://symthic.com/bf4-weapon-info?w=AUG_A3'),
 		'U_SteyrAug_M320_LVG' => array('AUG A3 M320 LVG', 'description', 'steyraug_fancy.png','http://symthic.com/bf4-weapon-info?w=AUG_A3'),
-		'U_M26Mass_Flechette' => array('M26 DART', 'description', 'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
-		'U_M26Mass_Frag' => array('M26 FRAG', 'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
-		'U_M26Mass_Slug' => array('M26 SLUG', 'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
-		'U_M320_SHG' => array('M320 DART', 'description', 'm320_fancy.png'),
-		'U_M320_FLASH' => array('M320 FB', 'description', 'm320_fancy.png'),
-		'U_M320_HE' => array('M320 HE', 'description', 'm320_fancy.png'),
-		'U_M320_LVG' => array('M320 LVG', 'description', 'm320_fancy.png'),
-		'U_M320_SMK' => array('M320 SMK', 'description', 'm320_fancy.png'),
+		'U_M26Mass_Flechette' => array('M26 DART', 'Compact shotgun capable of being mounted below the barrel of some Assault Rifles.<br />Loaded with flechette rounds which reduce damage but increase range and penetration. ', 'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
+		'U_M26Mass_Frag' => array('M26 FRAG', 'Compact shotgun capable of being mounted below the barrel of some Assault Rifles.<br />Loaded with explosive FRAG ammunition for increased suppressing power.',  'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
+		'U_M26Mass_Slug' => array('M26 SLUG', 'Compact shotgun capable of being mounted below the barrel of some Assault Rifles.<br /> Loaded with SABOT slug rounds for accurate medium range fire.', 'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
+		'U_M26Mass' => array('M26 Mass', 'Compact shotgun capable of being mounted below the barrel of some Assault Rifles.br />Loaded with standard buckshot with high damage but no penetration.', 'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
+		'U_M320_SHG' => array('M320 DART', 'Flechettes packed in a 40mm cartridge that effectively transforms the<br />launcher into a shotgun.', 'm320_fancy.png'),
+		'U_M320_FLASH' => array('M320 FB', 'Flash Bang 40mm grenade with a suppressive flash to temporarily <br />blind enemies in close quarters.', 'm320_fancy.png'),
+		'U_M320_HE' => array('M320 HE', 'High Explosive 40mm grenade with a small blast radius for engaging grouped infantry and light vehicles.', 'm320_fancy.png'),
+		'U_M320_LVG' => array('M320 LVG', 'Timed 40mm grenade with a small anti-personnel warhead that bounces<br />off surfaces before exploding. ', 'm320_fancy.png'),
+		'U_M320_SMK' => array('M320 SMK', '40mm grenade that creates a blinding cloud of white smoke on impact which also blocks spotting.', 'm320_fancy.png'),
 		'U_M416_M26_Buck' => array('M416 M26 Buckshot', 'description', 'm416_fancy.png','http://symthic.com/bf4-weapon-info?w=M416'),
 		'U_M416_M26_Flechette' => array('M416 M26 Flechette', 'description', 'm416_fancy.png','http://symthic.com/bf4-weapon-info?w=M416'),
 		'U_M416_M26_Frag' => array('M416 M26 Fragment', 'description', 'm416_fancy.png','http://symthic.com/bf4-weapon-info?w=M416'),
@@ -114,9 +114,9 @@ $config = array(
 		'U_SCAR-H_M26_Flechette' => array('SCAR-H M26 Flechette', 'description', 'scarh_fancy.png','http://symthic.com/bf4-weapon-info?w=SCAR-H'),
 		'U_SCAR-H_M320_HE' => array('SCAR-H M320 HE', 'description', 'scarh_fancy.png','http://symthic.com/bf4-weapon-info?w=SCAR-H'),
 		'U_USAS-12_Nightvision' => array('USAS-12 FLIR', 'description', 'usas12_nv_fancy.png'),
-		'U_XM25' => array('XM25 AIRBURST', 'description', 'xm25_fancy.png'),
-		'U_XM25_Flechette' => array('XM25 DART', 'description', 'xm25_fancy.png'),
-		'U_XM25_Smoke' => array('XM25 SMOKE', 'description', 'xm25_fancy.png'),
+		'U_XM25' => array('XM25 AIRBURST', 'Fires 25mm grenades that can explode mid-flight creating an airburst effect to eliminate targets behind cover.<br/>Aiming down the sights at a cover will lock in that distance,<br />allowing the grenade to explode in the air 3 meters past the cover. ', 'xm25_fancy.png'),
+		'U_XM25_Flechette' => array('XM25 DART', 'Fires a 25mm cartridge packed with penetrating Flechettes that effectively transform the launchery<br />into a semiautomatic shotgun. ', 'xm25_fancy.png'),
+		'U_XM25_Smoke' => array('XM25 SMOKE', 'Fires 25mm grenades that can explode mid-flight creating an airburst smoke effect to block sight and spotting.<br />Aiming down the sights at a cover will lock in that distance,<br /> allowing the grenade to explode in the air 3 meters past the cover. ', 'xm25_fancy.png'),
 
 		// Carbines
 		'U_A91' => array('A91', 'description', 'a91_fancy.png','http://symthic.com/bf4-weapon-info?w=A-91'),
@@ -154,6 +154,7 @@ $config = array(
 		'U_M200' => array('SSR-61', 'description', 'm200_srr61_fancy.png','http://symthic.com/bf4-weapon-info?w=SRR-61'),
 		'U_SV98' => array('SV98', 'description', 'sv98_fancy.png','http://symthic.com/bf4-weapon-info?w=SV98'),
 		'U_GOL' => array('GOL Magnum', 'description', 'gol_fancy.png','http://symthic.com/bf4-weapon-info?w=GOL_Magnum'),
+		'U_SR338' => array('338-RECON', 'description', 'srs_338recon_fancy.png','http://symthic.com/bf4-weapon-info?w=338-Recon'),
 
 		// LMG
 		'U_LSAT' => array('LSAT', 'description', 'lsat_fancy.png','http://symthic.com/bf4-weapon-info?w=LSAT'),
@@ -193,7 +194,6 @@ $config = array(
 		'U_SerbuShorty' => array('Shorty 12GA', 'description', 'shorty12g_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
 		'U_SPAS12' => array('SPAS-12', 'description', 'spas12_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
 		'U_UTAS' => array('UTS 15', 'description', 'utas_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
-		'U_M26Mass' => array('M26 MASS', 'description', 'm26mass_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
 		'U_DAO12' => array('DAO-12', 'description', 'dao12_fancy.png','http://symthic.com/bf4-shotgun-stats?p=stats'),
 		'U_SR2' => array('SR-2', 'description', 'sr2_fancy.jpg','http://symthic.com/bf4-shotgun-stats?p=stats'),
 			
@@ -213,16 +213,16 @@ $config = array(
 		'U_SW40' => array('SW 40', 'description', 'sw40_fancy.png'),
 
 		// Grenades
-		'U_Flashbang' => array('Flashbang', 'description', 'm84_flashbang_fancy.png'),
-		'U_M34' => array('M34 INCENDIARY', 'description', 'm34_incendiary_fancy.png'),
-		'U_M67' => array('M67 FRAG', 'description', 'm67_grenade_fancy.png'),
-		'U_Grenade_RGO' => array('RGO IMPACT', 'description', 'rgo_impact_fancy.png'),
-		'U_V40' => array('V40 MINI', 'description', 'v40_mini_fancy.png'),
+		'U_Flashbang' => array('Flashbang', 'Hand grenade with a suppressive Flash Bang effect to temporarily blind enemies in close quarters.', 'm84_flashbang_fancy.png'),
+		'U_M34' => array('M34 INCENDIARY', 'Incendiary hand grenade which creates a cloud of intense burning particles for a short duration.<br />Particles stick to soldiers and will continue to burn outside the original fire.', 'm34_incendiary_fancy.png'),
+		'U_M67' => array('M67 FRAG', 'Standard timed fuze hand grenade with all around performance and balanced range and damage.', 'm67_grenade_fancy.png'),
+		'U_Grenade_RGO' => array('RGO IMPACT', 'Russian RGO Impact grenade which explodes shortly after impacting a surface. <br />A smaller grenade with a lower blast yield, two of these grenades can be carried at one time. ', 'rgo_impact_fancy.png'),
+		'U_V40' => array('V40 MINI', 'Mini hand grenade that can be thrown further than the standard M67 but with reduced blast yield.<br />3 of these grenades can be carried.', 'v40_mini_fancy.png'),
 
 		// map pick up
 		'U_AMR2' => array('AMR-2', 'description', 'amr_far_fancy.png'),
 		'U_AMR2_MED' => array('AMR-2 MID', 'description', 'amr_med_fancy.png'),
-		'U_AT4' => array('M136 CS', 'description', 'm136_fancy.png'),
+		'U_AT4' => array('M136 CS', 'Confined Space version of the M136, capable of being fired indoors without harming the shooter.<br />A capable dumb fire anti-armor and anti-personnel weapon.', 'm136_fancy.png'),
 		'U_AMR2_CQB' => array('AMR-2 CQB', 'description', 'amr_cqb_fancy.png'),
 		'U_M136' => array('M136 CS', 'description', 'm136_fancy.png'),
 		'U_MGL' => array('M32 MGL', 'description', 'mgl_fancy.png'),
@@ -230,34 +230,35 @@ $config = array(
 		'U_M82A3_CQB' => array('M82A3 CQB', 'description', 'm82a3_cqb_fancy.png'),
 		'U_M82A3_MED' => array('M82A3 MID', 'description', 'm82a3_med_fancy.png'),
 		'U_USAS-12' => array('USAS-12', 'description', 'usas12_bp_fancy.png'),
-
+		
 		// Gadgets
-		'U_C4' => array('C4', 'description', 'c4_fancy.png'),
-		'U_C4_Support' => array('C4', 'description', 'c4_fancy.png'),
-		'U_Defib' => array('Defibrillator', 'description', 'defib_fancy.png'),
-		'U_Claymore' => array('M18 CLAYMORE', 'description', 'claymore_fancy.png'),
-		'U_Claymore_Recon' => array('M18 CLAYMORE', 'description', 'claymore_fancy.png'),
-		'U_FGM148' => array('FGM-148 JAVELIN', 'description', 'fgm148_javelin_fancy.png'),
-		'U_SRAW' => array('FGM-172 SRAW', 'description', 'fgm172_sraw_fancy.png'),
-		'U_SMAW' => array('MK153 SMAW', 'description', 'mk153_smaw_fancy.png'),
-		'U_FIM92' => array('FIM-92 Stinger', 'description', 'fim92_stinger_fancy.png'),
-		'U_RPG7' => array('RPG-7V2', 'description', 'rpg7_fancy.png'),
-		'U_Sa18IGLA' => array('SA-18 IGLA', 'description', 'sa18_igla_fancy.png'),
+		'U_C4' => array('C4', 'Plastic explosives that stick to most surfaces. Capable of a Mobility Kill on vehicles, and the remote detonator<br /> allows for traps and ambushes.', 'c4_fancy.png'),
+		'U_C4_Support' => array('C4', 'Plastic explosives that stick to most surfaces. Capable of a Mobility Kill on vehicles, and the remote detonator<br /> allows for traps and ambushes. ', 'c4_fancy.png'),
+		'U_Defib' => array('Defibrillator', 'Automated External Defibrillator (AED) revives downed teammates and electrocutes enemies.<br />Charging the paddles will revive teammates with increased health.<br />Needs to recharge after multiple quick uses. ', 'defib_fancy.png'),
+		'U_Claymore' => array('M18 CLAYMORE', 'Anti-personnel mine that launches 3 trip wires shortly after being deployed. Breaking a wire<br />will detonate the mine. A mine with no trip wires is unable to detonate and should be redeployed.', 'claymore_fancy.png'),
+		'U_Claymore_Recon' => array('M18 CLAYMORE', 'Anti-personnel mine that launches 3 trip wires shortly after being deployed.<br/>Breaking a wire will detonate the mine. A mine with no trip wires is unable to detonate and should be redeployed.', 'claymore_fancy.png'),
+		'U_FGM148' => array('FGM-148 JAVELIN', 'Guided Anti-Tank missile launcher that locks on to land vehicles. Warhead does medium damage to armor from any angle of attack.<br /> Launcher must maintain lock until the missile hits the target.', 'fgm148_javelin_fancy.png'),
+		'U_SRAW' => array('FGM-172 SRAW', 'The Wire-Guided Anti-Tank missile launcher can be manually guided to the target as long as<br /> the launcher is in aimed mode. Capable of locking on to laser-designated targets.', 'fgm172_sraw_fancy.png'),
+		'U_SMAW' => array('MK153 SMAW', 'High speed, low drag Anti-Vehicle launcher with flatter trajectory but lower damage than the RPG-7V2.<br />Most effective against the sides and rear of armored targets.', 'mk153_smaw_fancy.png'),
+		'U_FIM92' => array('FIM-92 Stinger', 'Fire and Forget Anti-Air missile capable of Mobility Kills on most aircraft at short range.<br />STINGER missiles will guide themselves to the target after launch.', 'fim92_stinger_fancy.png'),
+		'U_RPG7' => array('RPG-7V2', 'Powerful rocket-propelled Anti-Vehicle launcher capable of disabling even heavily armored vehicles<br />from the sides and rear. ', 'rpg7_fancy.png'),
+		'U_Sa18IGLA' => array('SA-18 IGLA', 'Anti-Air missile capable of Mobility Kills on most aircraft at medium range.<br />Launcher must maintain lock until the missile hits the target.', 'sa18_igla_fancy.png'),
 		'U_PortableMedicpack' => array('FIRST AID PACK', 'description', 'medicpack.png'),
 		'U_Medkit' => array('MEDIC BAG', 'description', 'medicbag.png'),
-		'U_Starstreak' => array('HVM-II', 'description', 'hvm_starstreak_fancy.png'),
-		'U_M15' => array('M15 AT MINE', 'description', 'at_mine_fancy.png'),
-		'U_SLAM' => array('M2 SLAM', 'description', 'm2_slam_fancy.png'),
-		'U_Repairtool' => array('REPAIR TOOL', 'description', 'repairtool_fancy.png'),
+		'U_Starstreak' => array('HVM-II', 'Fires a cluster of 3 projectiles that are highly effective against both Aircraft and Light Vehicles.<br/>Launcher must maintain lock until the missile hits the target.<br /> Capable of locking on to laser designated targets. ', 'hvm_starstreak_fancy.png'),
+		'U_M15' => array('M15 AT MINE', 'High Explosive Anti-Tank mine that detonates when vehicles pass by in close proximity.<br />Capable of a Mobility Kill on even the heaviest armored vehicles.', 'at_mine_fancy.png'),
+		'U_SLAM' => array('M2 SLAM', 'Selectable Lightweight Attack Munition usable as either an off-route Anti-Tank mine or a traditional land mine.<br />Blast damage is less than the M15, but is still capable of a<br /> Mobility Critical on heavily armored vehicles. ', 'm2_slam_fancy.png'),
+		'U_Repairtool' => array('REPAIR TOOL', 'Hand held oxy-fuel welding and cutting torch that repairs friendly vehicles and damages enemy vehicles and infantry.', 'repairtool_fancy.png'),
+		'U_M320_3GL'  => array('M320 3GL', 'Fires 3 smaller 40mm grenades for attacking several targets with less<br />damage than the HE grenade.', 'm320_3gl_fancy.png', ''),
 
 		// Commander
-		'U_Tomahawk' => array('CRUISE MISSILE', 'description', 'image.png'),
+		'U_Tomahawk' => array('CRUISE MISSILE', 'Commander Attack Weapon', 'image.png'),
 
 		// Knifes
 		'Melee' => array('Melee', 'description', 'knife_bowie_fancy.png'),
 
 		// misc
-		'U_NLAW' => array('MBT LAW', 'description', 'mbt_law_fancy.png'),
+		'U_NLAW' => array('MBT LAW', 'Smart Anti-Tank missile that automatically detects vehicles near the warhead and guides to the target.<br />Easy to use but low damage from all angles of attack.<br />Capable of locking on to laser-designated targets. ', 'mbt_law_fancy.png'),
 
 		//Vehicles and new weapon Images since Dice just put out the names of Vehicle Deaths,Kills
 		'Gameplay/Vehicles/M1A2/M1Abrams' => array('M1 ABRAMS', 'description', 'm1a2_fancy.png'),
@@ -286,17 +287,17 @@ $config = array(
 		'Gameplay/Vehicles/CH_FAC_DV15/CH_FAC_DV15' => array('DV-15 CH', 'description' , 'dv15_fancy.png'),
 		'Gameplay/Vehicles/CH_FAC_DV15/spec/CH_FAC_DV15_RU' => array('DV-15 RU', 'description' , 'dv15_fancy.png'),
 		'Gameplay/Vehicles/A-10_THUNDERBOLT/A10_THUNDERBOLT'=> array('A10 WARTHOG', 'description' , 'a10_fancy.png'),
-		'XP1/Gameplay/Gadgets/UCAV/UCAV_Launcher' => array('UCAV', 'description', 'ucav_fancy.png'),
+		'XP1/Gameplay/Gadgets/UCAV/UCAV_Launcher' => array('UCAV', 'A small reconnaissance unmanned combat areal vehicle armed with an explosive warhead<br />that allows the support class to use it as guided munitions. ', 'ucav_fancy.png'),
 		'RHIB' => array('RHIB BOAT', 'description', 'rhib_fancy.png'),
 		'Gameplay/Vehicles/Ch_FJET_J-20/CH_FJET_J-20' => array('J-20', 'description', 'j20_fancy.png'),
 		'Gameplay/Vehicles/Venom/Venom' => array('UH-1Y VENOM', 'description', 'venom_fancy.png'),
 		'Gameplay/Vehicles/Pantsir/Pantsir-S1' => array('PANTSIR-S1', 'description', 'pantsir_fancy.png'),
 		'DPV' => array('DPV', 'description', 'dpv_fancy.png'),
 		'XP1/Gameplay/Vehicles/H6K/H6K' => array('Bomber', 'description', 'h6k_fancy.png'),
-		'AA Mine' => array('Anti Air Mine', 'description', 'aamine_fancy.png'),
+		'AA Mine' => array('Anti Air Mine', 'Anti-Air Mine which targets air vehicles flying within its radius.', 'aamine_fancy.png'),
 		'Gameplay/Vehicles/US_FAC-CB90/US_FAC-CB90' => array('RCB', 'description', 'rcb90_fancy.png'),
 		'Gameplay/Vehicles/CH_ATGM_HJ-8/CH_ATGM_HJ-8' => array('HJ-8 LAUNCHER', 'description', 'hj8_fancy.png'),
-		'M224' => array('M224 MORTAR', 'description', 'm224_mortar_fancy.png'),
+		'M224' => array('M224 MORTAR', 'Remote-controlled 60mm Mortar that fires high explosive rounds using a terrain grid targeting system.<br />Shells travel a ballistic path and may hit objects between the launcher and the intended target.<br />Rapid fire is very inaccurate, single shells have great precision.', 'm224_mortar_fancy.png'),
 		'Gameplay/Vehicles/GunShieldAndTripod/GunShieldTripod' => array('.50 CAL', 'description', 'gunshield_fancy.png'),
 		'Gameplay/Vehicles/CH_MRAP-ZFB-05/CH_MRAP-ZFB-05' => array('MRAP', 'description', 'mrap-cougar_fancy.png'),
 		'Gameplay/Vehicles/AAV-7A1/AAV-7A1' => array('AAV-7A1 AMTRAC', 'description', 'aav_fancy.png'),
@@ -309,7 +310,7 @@ $config = array(
 		'Ka-60' => array('Ka-60 Kasatka', 'description', 'ka60_fancy.png'),
 		'XP1/Gameplay/Vehicles/B1Lancer/B1Lancer' => array('B1 Lancer', 'description', 'image.png'),
 		'Gameplay/Vehicles/GrowlerITV/GrowlerITV' => array('M1161 ITV', 'description', 'growler_fancy.png', ''),
-		'U_M320_3GL'  => array('M320 3GL', 'description', 'm320_3gl_fancy.png', ''),
+
 		
 		/* Example */
 		//'' => array('', 'description', '', ''),
